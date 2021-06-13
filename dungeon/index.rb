8439cf4b0d259484d8990c70f726826ee6b2c6c9
justinsalasdev <justinsalasdev@gmail.com>
2021-06-13 12:17:28 +0800

    added initial lines

---
+++ dungeon/index.rb
@@ -0,0 +1,48 @@
+class Room 
+    attr_accesor :reference, :name, :description, :connections
+    def initialize(reference, name, description, connections)
+        @reference = reference
+        @name = name
+        @description = description
+        @connections = connections
+    end
+end
+
+
+
+class Dungeon
+    attr_accesor :player
+
+    def initialize player
+        @player = player
+        @rooms = {}
+    end
+
+    def add_room(reference, name, description, connections)
+        @rooms[reference] = Room.new(reference,name,description, connections)
+    end
+
+    def start location
+        @player.location = location
+        show_current_description
+    end
+
+    def show_current_description
+        puts find_room_in_dungeon(@player.location).full_description
+    end
+
+    def find_room_in_dungeon reference
+        @rooms[reference]
+    end
+
+    def find_room_in_direction
+        find_room_in_dungeon(@player.location).connections[direction]
+    end
+
+
+    def go direction
+        puts "You go " + direction.to_s
+        @player.location = find_room_in_direction(direction)
+        show_current_description
+    end
+end
