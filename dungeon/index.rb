f7dd1f6d6bc0af0438a241d7f594e523daa50693
justinsalasdev <justinsalasdev@gmail.com>
2021-06-13 15:32:29 +0800

    added debugger config

--- dungeon/index.rb
+++ dungeon/index.rb
@@ -13,7 +13,7 @@ end
 class Dungeon
     attr_accessor :player
 
-    def initialize player
+    def initialize player 
         @player = player
         @rooms = {}
     end
@@ -29,7 +29,7 @@ class Dungeon
 
 
     def show_current_description
-        puts find_room_in_dungeon(@player.location).full_description
+        puts find_room_in_dungeon(@player.location).get_full_description
     end
 
     def start location
@@ -37,7 +37,7 @@ class Dungeon
         show_current_description
     end
 
-    def find_room_in_direction
+    def find_room_in_direction direction
         find_room_in_dungeon(@player.location).connections[direction]
     end
 
@@ -48,4 +48,36 @@ class Dungeon
         show_current_description
     end
 end
+
+
+class Player
+    attr_accessor :name, :location
+    def initialize name
+        @name = name
+    end
+end
+
+
+class Room
+    attr_accessor :reference, :name, :description, :connections
+    def initialize(reference,name,description,connections)
+        @reference = reference
+        @name = name
+        @description = description
+        @connections = connections
+    end
+
+    def get_full_description
+        @name + "\n\nYou are in " + @description
+    end
+end
+
+player = Player.new("Fred Bloggs")
+dungeon = Dungeon.new(player)
+
+
+#Add rooms to the dungeon
+dungeon.add_room(:largecave,"Large Cave","a large cave", {west: :smallcave})
+dungeon.add_room(:smallcave,"Small Cave","a small, claustrophobic cave", {east: :largecave})
+dungeon.start(:largecave)
 5;
